1ec108e5c1b71624e90b8f1e1b63feefa5d46581
JohnnyMartin <1627710428@qq.com>
2018-01-18 16:28:20 +0800

    删掉无用的注释

--- source/core/luaenv/DllHelper.h
+++ source/core/luaenv/DllHelper.h
@@ -5,12 +5,6 @@ Date:				2017.12.15
 Description:		在Lua中调用dll
 *****************************************************/
 
-/*
-1，如果所有的Lua文件都共用一个LuaState运行，回掉函数等极易重名。所以我们让每个Lua文件都有一个自己的LuaState。
-这样做带来一个问题：全局变量、全局函数不易共享。所以得有一个LuaEnv来管理这些luastate,处理横跨luastate的
-全局变量。
-2，第一版本，所有lua文件都运行在主线程，ENV单例。
-*/
 #pragma once
 #include "../stdafx.h"
 #include "LuaAPI.h"
